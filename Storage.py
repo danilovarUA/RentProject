b66027afadd6e9614b3cefe173c3c2e752f0f893
Dan <dan.varushchik@gmail.com>
2020-09-14 17:10:13 +0300

    Stoage upgrade: - now agreement and property can be noth added and updated - moving table names out of the code - SQL errors are now more informative - agreement and propery can be gotten by id

--- Storage.py
+++ Storage.py
@@ -1,6 +1,8 @@
 import sqlite3
 
 DATABASE_NAME = 'rentDatabase.sqlite'
+AGREEMENTS_TABLE_NAME = "agreements"
+PROPERTIES_TABLE_NAME = "properties"
 
 
 class Database:
@@ -8,37 +10,28 @@ class Database:
         self.connection = sqlite3.connect(DATABASE_NAME)
         self.cursor = self.connection.cursor()
 
-    def add_agreement(self, fields):
-        if not(
-            validate_string(fields["company"]) or
-            validate_string(fields["person"]) or
-            validate_number(fields["recovery_price"]) or
-            validate_date(fields["last_accept_day"]) or
-            validate_number(fields["first_payment"]) or
-            validate_number(fields["last_same_payment"]) or
-            validate_date(fields["start_day"]) or
-            validate_date(fields["end_day"])
-        ):
+    def set_agreement(self, fields, index):
+        if not(validate_string(fields["company"]) or validate_string(fields["person"]) or
+               validate_number(fields["recovery_price"]) or validate_date(fields["last_accept_day"]) or
+               validate_number(fields["first_payment"]) or validate_number(fields["last_same_payment"]) or
+               validate_date(fields["start_day"]) or validate_date(fields["end_day"])):
             return False
-        query = ("INSERT INTO agreements(" +
-                 "company, " +
-                 "person, " +
-                 "recovery_price," +
-                 "last_accept_day," +
-                 "first_payment," +
-                 "last_same_payment," +
-                 "start_day," +
-                 "end_day" +
-                 ") VALUES (" +
-                 "'{}', ".format(fields["company"]) +
-                 "'{}', ".format(fields["person"]) +
-                 "{}, ".format(fields["recovery_price"]) +
-                 "'{}', ".format(fields["last_accept_day"]) +
-                 "'{}', ".format(fields["first_payment"]) +
-                 "{}, ".format(fields["last_same_payment"]) +
-                 "'{}', ".format(fields["start_day"]) +
-                 "'{}' ".format(fields["end_day"]) +
-                 ")")
+
+        if index == -1:
+            query = ("INSERT INTO {}(company, person, recovery_price, last_accept_day, ".format(AGREEMENTS_TABLE_NAME) +
+                     "first_payment, last_same_payment, start_day, end_day) " +
+                     "VALUES ({}, {}, {}, {}, {}, {}, {}, {})".format(
+                         fields["company"], fields["person"], fields["recovery_price"], fields["last_accept_day"],
+                         fields["first_payment"], fields["last_same_payment"], fields["start_day"], fields["end_day"]))
+        else:
+            query = ("UPDATE {} SET ".format(AGREEMENTS_TABLE_NAME) +
+                     "company = '{}', person = '{}', recovery_price = '{}', ".format(
+                         fields["company"], fields["person"], fields["recovery_price"]) +
+                     "last_accept_day = '{}', first_payment = '{}', last_same_payment = '{}', ".format(
+                       fields["last_accept_day"], fields["first_payment"], fields["last_same_payment"]) +
+                     "start_day = '{}', end_day = '{}' ".format(
+                       fields["start_day"], fields["end_day"]) +
+                     "WHERE id={}".format(index))
         return self._execute_(query)
 
     def assign_properties(self):
@@ -50,9 +43,10 @@ class Database:
         try:
             self.cursor.execute(query)
             self.connection.commit()
-        except sqlite3.OperationalError:
-            print("DBError: ({})".format(query))
+        except sqlite3.OperationalError as e:
+            print("DBError[{}]: ({})".format(e, query))
             return False
+        print("Done: {}".format(query))
         return True
 
     def remove_agreements(self, ids):
@@ -61,47 +55,39 @@ class Database:
                 return False
         return True
 
-    def change_agreement(self, agreement_id, fields):
-        #self.connection.commit()
-        raise ValueError("Function not finished")
-
-    def get_agreements(self):
-        self._execute_("SELECT * FROM agreements")
+    def get_agreements(self, index=None):
+        if index is None:
+            self._execute_("SELECT * FROM agreements")
+        else:
+            self._execute_("SELECT * FROM agreements WHERE id={}".format(index))
         return self.cursor.fetchall()
 
-    def add_property(self, fields):
-        if not (
-                validate_string(fields["name"]) or
-                validate_string(fields["address"]) or
-                validate_number(fields["area"]) or
-                validate_date(fields["given_day"]) or
-                validate_number(fields["agreement_id"])
-        ):
+    def set_property(self, fields, index):
+        # TODO finish
+        if not (validate_string(fields["name"]) or validate_string(fields["address"]) or
+                validate_number(fields["area"]) or validate_date(fields["given_day"]) or
+                validate_number(fields["agreement_id"])):
             return False
-        query = ("INSERT INTO properties(" +
-                 "name, " +
-                 "address, " +
-                 "area, " +
-                 "given_day, " +
-                 "agreement_id" +
-                 ") VALUES (" +
-                 "'{}', ".format(fields["name"]) +
-                 "'{}', ".format(fields["address"]) +
-                 "{}, ".format(fields["area"]) +
-                 "'{}', ".format(fields["given_day"]) +
-                 "{} ".format(fields["agreement_id"]) +
-                 ")")
+
+        if index == -1:
+            query = ("INSERT INTO {}(name, address, area, given_day, agreement_id)".format(PROPERTIES_TABLE_NAME) +
+                     "VALUES ({}, {}, {}, {}, {})".format(
+                         fields["name"], fields["address"], fields["area"], fields["given_day"],
+                         fields["agreement_id"]))
+        else:
+            query = ("UPDATE {} SET ".format(PROPERTIES_TABLE_NAME) +
+                     "name = {}, address = {}, area = {}, ".format(
+                       fields["company"], fields["person"], fields["recovery_price"]) +
+                     "given_day = {}".format(fields["last_accept_day"]) +
+                     "WHERE id={}".format(index))
+
         return self._execute_(query)
 
     def remove_properties(self, ids, agreement_ids):
         # self.connection.commit()
         raise ValueError("Function not finished")
 
-    def change_property(self, property_id, fields):
-        # self.connection.commit()
-        raise ValueError("Function not finished")
-
-    def get_properties_by_agreement(self, agreement_id):
+    def get_properties(self, agreement_id):
         self._execute_("SELECT * FROM properties WHERE agreement_id = {}".format(agreement_id))
         return self.cursor.fetchall()
 
@@ -121,8 +107,8 @@ def validate_date(value):
     if not isinstance(value, str):
         return False
     day, month, year = value.split("/")
-    return (day.isnumeric() and len(day) <= 2
-            and month.isnumeric() and len(month) <= 2
+    return (day.isnumeric() and len(day) <= 2 and day in range(0, 32)
+            and month.isnumeric() and len(month) <= 2 and month in range(0, 13)
             and year.isnumeric() and len(year) == 4)
 
 
@@ -139,7 +125,7 @@ if __name__ == "__main__":
     #                   "start_day": "начало 12ю12ю1212",
     #                   "end_day": "конец 12ю12ю1212"
     # })
-    db.add_property({"name": "erkekmvl",
+    db.set_property({"name": "erkekmvl",
                      "address": "Uthjtd Rhen 12",
                      "area": 1001234,
                      "given_day": "10/10/2010",
